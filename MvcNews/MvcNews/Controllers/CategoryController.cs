d47a0240a462f872c7c4eea3b6cec051392ebdc4
kaplankubilay1 <kubilay_kaplan@outlook.com>
2020-08-19 18:31:56 +0300

    first commit

---
+++ MvcNews/MvcNews/Controllers/CategoryController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcNews.Models;
+
+namespace MvcNews.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private NewsDbEntities _db = new NewsDbEntities();
+
+        // GET: Category
+        public ActionResult Index()
+        {
+            List<SelectCategoryObject> listCategory = _db.sp_SelectCategories().ToList();
+            return View(listCategory);
+        }
+
+        // GET: Category/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Category/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                _db.sp_InsertCategories(collection["CategoryName"]);
+                return RedirectToAction("Index");
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Category-Create-Post is take error.");
+                return View();
+            }
+        }
+
+        // GET: Category/Delete
+        public ActionResult Delete(int cid)
+        {
+            ViewBag.error = "";
+            SelectCategoryObject selectCategoryObject = _db.sp_SelectCategoryById(cid).First();
+            if (selectCategoryObject==null)
+            {
+                return HttpNotFound();
+            }
+            return View(selectCategoryObject);
+        }
+
+        // POST: Category/Delete
+        [HttpPost]
+        public ActionResult Delete(int cid, FormCollection collection)
+        {
+            try
+            {
+               var listNews = _db.sp_SelectNews().Where(p => p.CId == cid).FirstOrDefault();
+                if (listNews!=null)
+                {
+                    SelectCategoryObject selectCategoryObject = _db.sp_SelectCategoryById(cid).First();
+
+                    ViewBag.error = "Kategoriye ait içerik mevcut silinemez!.";
+                    return View(selectCategoryObject);
+
+                }
+                _db.sp_DeleteCategories(cid);
+                return RedirectToAction("Index");
+            }
+            catch(Exception)
+            {
+                ViewBag.error = "";
+                Console.WriteLine("Category-Delete-Post is take error.");
+                return View();
+            }
+        }
+    }
+}
---
+++ MvcNews/MvcNews/Controllers/NewsController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcNews.Models;
+
+namespace MvcNews.Controllers
+{
+    public class NewsController : Controller
+    {
+        //Create DB instance
+        private NewsDbEntities _db = new NewsDbEntities();
+
+        // GET: News
+        public ActionResult Index()
+        {
+            List <SelectNewsObject> listNews= _db.sp_SelectNews().ToList();
+            return View(listNews);
+        }
+
+
+        //POST:/News/Index
+        [HttpPost]
+        public ActionResult Index(int categoryName)
+        {
+            List<SelectNewsObject> listNews = new List<SelectNewsObject>();
+            try
+            {
+                listNews = _db.sp_SelectNews().Where(p => p.CId == categoryName).ToList();
+                if (listNews == null)
+                {
+                    return View(listNews);
+                }
+            }
+            catch (Exception)
+            {                
+                Console.WriteLine("News-SearcText-Post is take error.");
+            }
+            return View(listNews);
+        }
+
+        // GET: News/Create
+        public ActionResult Create()
+        {
+            ViewBag.Categories = _db.sp_SelectCategories().ToList();
+            return View();
+        }
+
+        //POST: News/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                if (collection["cId"]==null)
+                {
+                    return RedirectToAction("Index");
+                }
+                var cId = Convert.ToInt32(collection["cId"]);   
+                _db.sp_InsertNews(cId, collection["NewsTitle"], collection["NewsContent"]);
+                return RedirectToAction("Index");
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("News-Create-Post is take error.");
+                ViewBag.Categories = _db.sp_SelectCategories().ToList();
+                return View();
+            }
+        }
+
+        // GET: News/Edit
+        public ActionResult Edit(int id)
+        {
+            SelectNewsObject selectNewsObject = _db.sp_SelectNewsById(id).First();
+            if (selectNewsObject == null)
+            {
+                return HttpNotFound();
+            }
+            return View(selectNewsObject);
+        }
+
+        // POST: News/Edit
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                SelectNewsObject selectNewsObject = _db.sp_SelectNewsById(id).First();
+                if (ModelState.IsValid)
+                {
+                    var cid = selectNewsObject.CId;
+                    _db.sp_UpdateNews(id, cid, collection["NewsTitle"], collection["NewsContent"]);
+                }
+                return RedirectToAction("Index");
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("News-Edit-Post is take error.");
+                return View();
+            }
+        }
+
+        // GET: News/Delete
+        public ActionResult Delete(int id)
+        {
+            SelectNewsObject selectNewsObject = _db.sp_SelectNewsById(id).First();
+            if (selectNewsObject == null)
+            {
+                return HttpNotFound();
+            }
+            return View(selectNewsObject);
+        }
+
+        // POST: News/Delete
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                _db.sp_DeleteNews(id);
+                return RedirectToAction("Index");
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("News-Delete-Post is take error.");
+                return View();
+            }
+        }
+    }
+}
